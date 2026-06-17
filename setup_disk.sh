32a9071251dc904653193eed4ea3bc18dc173bf1
jit06 <julien@bluemind.org>
2026-06-17 21:58:15 +0200

    the script does no more require to be launched as root

--- setup_disk.sh
+++ setup_disk.sh
@@ -12,14 +12,14 @@
 
 set -e
 
-__DEPENDENCIES__=("wget" "unzip" "sed" "partprobe" "mount" "umount" "curl")
+__DEPENDENCIES__=("wget" "unzip" "sed" "partprobe" "mount" "umount" "curl" "sudo")
 CLEAN_UP=false
 
 source main.config
 source functions.sh
 
 usage() {
-    echo "Usage: sudo $0 --disk [DEVICE] --kickstart [ROM_PATH] [OPTIONS]"
+    echo "Usage: $0 --disk [DEVICE] --kickstart [ROM_PATH] [OPTIONS]"
     echo ""
     echo "Required:"
     echo "  -d, --disk        Path to disk device (e.g., /dev/sdc)"
@@ -30,6 +30,8 @@ usage() {
     echo "  -l, --cmdline     Path to custom cmdline.txt"
     echo "  --clean-on-close  Remove downloads and extractions after completion"
     echo "  -h, --help        Display this help message"
+    echo ""
+    echo "Note that this script requires root privileges via sudo to run certain commands."
     exit 1
 }
 
@@ -51,7 +53,6 @@ done
 [[ -z "$DISK_PATH"          ]] && usage
 [[ -z "$KICKSTART_PATH"     ]] && usage
 [[ ! -f "$KICKSTART_PATH"   ]] && log_error "Kickstart file not found."
-[[ $(id -u) -ne 0           ]] && log_error "This script must be run as root (sudo)."
 
 # ensure all needed programs are presents
 check_dependencies
@@ -83,7 +84,7 @@ sed "s|\[PATH_TO_DISK\]|$DISK_PATH|g" "$OLDPWD/partitions.config" > generated_pa
 
 # initialize disc and create Amiga partitions
 log_info "Initializing disk and creating partitions..."
-$HST_BIN script generated_partitions.config > /dev/null
+sudo $HST_BIN script generated_partitions.config > /dev/null
 if [[ $? -ne 0 ]]; then
     log_error "could not initialize disk with : $HST_BIN script generated_partitions.config"
 fi
@@ -97,16 +98,16 @@ cp "$KICKSTART_PATH" emu68_files/kick.rom
 
 # mount the just created emu68 partition 
 mkdir -p "$MOUNT_POINT"
-partprobe "$DISK_PATH"
+sudo partprobe "$DISK_PATH"
 sleep 2 
-mount "${DISK_PATH}1" "$MOUNT_POINT" || log_error "Failed to mount ${DISK_PATH}1"
+sudo mount "${DISK_PATH}1" "$MOUNT_POINT" || log_error "Failed to mount ${DISK_PATH}1"
 
 # copy emu68 files to partition
 log_info "Copying files to ${DISK_PATH}1..."
-cp -r emu68_files/* "$MOUNT_POINT/"
-sync
+sudo cp -r emu68_files/* "$MOUNT_POINT/"
+sudo sync
 
-umount "$MOUNT_POINT"
+sudo umount "$MOUNT_POINT"
 log_success "Emu68 installation complete."
 
 
